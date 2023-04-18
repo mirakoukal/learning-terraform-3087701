3ed1af11172f3ebe5b183c6b9d14a4cea7fc7ffe
mirakoukal <mkoukal@purestorage.com>
2023-04-18 10:54:42 +0200

    logs repair

--- main.tf
+++ main.tf
@@ -59,10 +59,6 @@ module "blog_alb" {
   subnets            = module.blog_vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
 
-  access_logs = {
-    bucket = "my-alb-logs"
-  }
-
   target_groups = [
     {
       name_prefix      = "blog-"
